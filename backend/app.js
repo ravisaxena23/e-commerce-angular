272f3b9672d50f5f2a74511fb187764f804c5a9f
Ravi Saxena <ravi.saxena@shl.com>
2023-04-25 19:47:58 +0530

    adding mongo schema

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,7 @@ mongoose.connect(process.env.MONGO_CONNECTION).then(()=>{
 }).catch((err)=>{
     console.log(err)
 })
+
 app.listen(3000,()=>{
     console.log("the server is running at http://localhost:3000")
 })

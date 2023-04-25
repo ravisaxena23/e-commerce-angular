6fbad2f9c2a70646b8af9cff03cf6380e34ec831
Ravi Saxena <ravi.saxena@shl.com>
2023-04-25 20:04:45 +0530

    adding intial routes

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,45 @@
 const express =  require('express')
-require('dotenv/config')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-const app = express()
+const cors = require('cors')
+require('dotenv/config')
 
+// file constants
 const apiBaseUrl=process.env.API_BASE_PREFIX
 
 // Middleware
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors())
+app.options('*',cors())
 
 app.get('/',(req,res)=>{
     res.send('app running successfully')
 })
 
+// routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+
+app.use(`${apiBaseUrl}/categories`, categoriesRoutes);
+app.use(`${apiBaseUrl}/products`, productsRoutes);
+app.use(`${apiBaseUrl}/users`, usersRoutes);
+app.use(`${apiBaseUrl}/orders`, ordersRoutes);
+
+
+
+// database connection
 mongoose.connect(process.env.MONGO_CONNECTION).then(()=>{
     console.log("Database connected")
 }).catch((err)=>{
     console.log(err)
 })
 
+
+// server
+const app = express()
 app.listen(3000,()=>{
     console.log("the server is running at http://localhost:3000")
 })

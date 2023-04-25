7b043471239583d400f1384342132a6c01b7c413
Ravi Saxena <ravi.saxena@shl.com>
2023-04-25 11:43:41 +0530

    adding database string

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,23 @@
 const express =  require('express')
-
+require('dotenv/config')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
 const app = express()
 
+const apiBaseUrl=process.env.API_BASE_PREFIX
+
+// Middleware
+app.use(express.json())
+app.use(morgan('tiny'))
 
 app.get('/',(req,res)=>{
     res.send('app running successfully')
+})
+
+mongoose.connect(process.env.MONGO_CONNECTION).then(()=>{
+    console.log("Database connected")
+}).catch((err)=>{
+    console.log(err)
 })
 app.listen(3000,()=>{
     console.log("the server is running at http://localhost:3000")
